fe4c562e1a928d3d0da5bed6545f0e11c1449b01
Luis Thiago Padilha <mailluisthiago@gmail.com>
2023-11-05 10:07:45 -0300

    chore: adding fake data

--- client/src/initialData.js
+++ client/src/initialData.js
@@ -1,106 +1,111 @@
 const data = {
-    name: "Luis Thiago Padilha",
-    title: "Software Engineer",
-    phone: "+55 (42) 99145-4275",
-    mail: "luisthiagop@gmail.com",
-    location: "Ponta Grossa, PR, Brazil",
-    github: "http://github.com/luisthiagop",
-    linkedin: "https://www.linkedin.com/in/luisthiagop/",
-    website: "http://google.com",
-    summary: "Resourceful Developer with 11 years of experience in designing and developing user interfaces, testing and training employees. Skilled at utilizing a wide variety of tools and programs to provide effective applications.",
+    name: "Alex Johnson",
+    title: "Senior Frontend Engineer",
+    phone: "+1 (555) 555-5555",
+    mail: "alex.johnson@example.com",
+    location: "Imaginary City, IM, Imaginary Country",
+    github: "http://github.com/alexjohnson",
+    linkedin: "https://www.linkedin.com/in/alexjohnson/",
+    website: "http://example.com/alexjohnson",
+    summary: "Passionate Senior Frontend Engineer with 8 years of experience in building modern web applications. Proven ability to lead development teams and deliver high-quality, user-friendly interfaces.",
     education: [
-    {
-        institution: "Ponta Grossa State University",
-        course: "Bacharelor of Computer Engineering",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec"
-    },
-    {
-        institution: "President Kennedy State College",
-        course: "Computer Science",
-        startYear: 2011,
-        startMonth: "Jan",
-        endYear: 2014,
-        endMonth: "Dec"
-    }
+        {
+            institution: "Imaginary Tech University",
+            course: "Master of Computer Science",
+            startYear: 2011,
+            startMonth: "Sep",
+            endYear: 2013,
+            endMonth: "May"
+        },
+        {
+            institution: "Fictional College",
+            course: "Bachelor of Software Engineering",
+            startYear: 2007,
+            startMonth: "Aug",
+            endYear: 2011,
+            endMonth: "Jun"
+        }
     ],
     professionalExperiences: [
-    {
-        company: "CI&T Software",
-        role: "Software Architect",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec",
-        actions: [
-            "Created and maintained 10 web applications for numerous national and foreign clients.",
-            "Ensured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
-            "Created and analyzed 500 unit test cases.",
-            "Developed python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.",
-            "Established and lead a team of 10 people; covering every key role in the early stages."
-        ]
-    },
-    {
-        company: "CI&T Software",
-        role: "Software Engineer",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec",
-        actions: [
-            "Created and maintained 10 web applications for numerous national and foreign clients.",
-            "Ensured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
-            "Created and analyzed 500 unit test cases.",
-            "Developed python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.",
-            "Established and lead a team of 10 people; covering every key role in the early stages."
-        ]
-    },
-    {
-        company: "CI&T Software",
-        role: "Software Engineer",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec",
-        actions: [
-            "Created and maintained 10 web applications for numerous national and foreign clients.",
-            "Ensured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
-            "Created and analyzed 500 unit test cases.",
-            "Developed python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.",
-            "Established and lead a team of 10 people; covering every key role in the early stages."
-        ]
-    },
+        {
+            company: "CodeCrafters",
+            role: "Lead Frontend Engineer",
+            startYear: 2016,
+            startMonth: "Jun",
+            endYear: 2021,
+            endMonth: "Dec",
+            actions: [
+                "Led a team of frontend engineers in the development of a cutting-edge e-commerce platform.",
+                "Implemented advanced animations and transitions for an enhanced user experience.",
+                "Collaborated closely with UX/UI designers to create intuitive and visually appealing interfaces.",
+                "Developed a custom state management system using Redux for improved application performance.",
+                "Optimized front-end codebase resulting in a 30% reduction in load times.",
+                "Introduced and implemented best practices for accessibility, ensuring compliance with WCAG standards."
+            ]
+        },
+        {
+            company: "InnoTech Innovations",
+            role: "Senior UI/UX Developer",
+            startYear: 2013,
+            startMonth: "Mar",
+            endYear: 2016,
+            endMonth: "May",
+            actions: [
+                "Designed and developed user interfaces for web applications, focusing on usability and accessibility.",
+                "Collaborated with cross-functional teams to gather requirements and deliver high-quality solutions.",
+                "Conducted code reviews and mentored junior developers to ensure code quality and best practices.",
+                "Implemented responsive designs, ensuring seamless user experiences across various devices.",
+                "Utilized CSS preprocessors (Sass/SCSS) to streamline and enhance stylesheets."
+            ]
+        },
+        {
+            company: "Tech Wizards",
+            role: "Frontend Developer",
+            startYear: 2011,
+            startMonth: "Sep",
+            endYear: 2013,
+            endMonth: "Feb",
+            actions: [
+                "Contributed to the development of a data visualization platform using D3.js and React.",
+                "Collaborated with backend developers to integrate frontend components with server-side logic.",
+                "Participated in Agile development processes, including sprint planning and daily stand-ups.",
+                "Implemented client-side form validation to improve user data integrity.",
+                "Integrated third-party APIs for enhanced functionality and data retrieval."
+            ]
+        }
     ],
     technicalSkills: [
-        "JavaScript",
-        "HTML",
-        "CSS",
         "React",
-        "Node.js",
-        "Express",
-        "paralelepipde"
+        "JavaScript (ES6+)",
+        "TypeScript",
+        "HTML5",
+        "CSS3",
+        "Sass/SCSS",
+        "Webpack",
+        "Jest",
+        "Enzyme",
+        "GraphQL"
     ],
     softSkills: [
-        "JavaScript",
-        "HTML",
-        "CSS",
-        "React",
-        "Node.js",
-        "Express",
-        "paralelepipde"
+        "Problem Solving",
+        "Team Leadership",
+        "Communication",
+        "Time Management",
+        "Adaptability"
     ],
     additionalSkills: [
         "Public Speaking",
-        "Writting"
+        "Technical Writing",
+        "UX/UI Design"
     ],
     languages: [
         "English",
-        "Portuguese"
+        "Spanish",
+        "German"
     ],
     certifications: [
-        "Microsoft Azure Fundamentals"
+        "Certified Frontend Developer",
+        "UX/UI Design Certification"
     ]
 }
 
