f388f34893d455fe88c6b8918b9e266640adb208
Luis Thiago Padilha <mailluisthiago@gmail.com>
2024-01-08 19:04:54 -0300

    Update My CV

--- client/src/initialData.js
+++ client/src/initialData.js
@@ -6,8 +6,8 @@ const data = {
     location: "Ponta Grossa, PR, Brazil",
     github: "http://github.com/luisthiagop",
     linkedin: "https://www.linkedin.com/in/luisthiagop/",
-    website: "http://google.com",
-    summary: "Resourceful Developer with 11 years of experience in designing and developing user interfaces, testing and training employees. Skilled at utilizing a wide variety of tools and programs to provide effective applications.",
+    website: null,
+    summary: "I have 7 years of experience in web development using a wide range of technologies, especially based on javascript such as Node.js. My main difference is being very flexible and having training focused on the entire software development cycle. In my last positions I was responsible for defining the best design and architectural decisions to ensure scalability.",
     education: [
     {
         institution: "Ponta Grossa State University",
@@ -30,70 +30,90 @@ const data = {
     {
         company: "CI&T Software",
         role: "Software Architect",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec",
+        startYear: 2021,
+        startMonth: "Nov",
+        endYear: "Now",
+        endMonth: "",
         actions: [
-            "Created and maintained 10 web applications for numerous national and foreign clients.",
-            "Ensured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
-            "Created and analyzed 500 unit test cases.",
-            "Developed python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.",
-            "Established and lead a team of 10 people; covering every key role in the early stages."
+            "Act in a full-stack squad as the Software Architect for a B2B digital channel of one of Brazil's largest telecommunication companies.",
+            "Act in the maintenance and development of approximately 20 Java-based microservices (Quarkus) and a similar number of Vue.js and Node.js microfrontends within the existing architecture.",
+            "Designed and helped develop the integration of facial biometrics into various channel functionalities for enhanced authorization mechanisms. Actually it's used at different applications and verify around of 100.000 biometric faces per month.",
+            "Engineered a robust application facilitating streamlined upload of diverse client documents during prospecting and ensuring there is no overload on the cluster network and resources.",
+            "Ensured the ongoing maintenance, robustness, health of the system modules using performing stress tests and Kibana, Grafana, Prometheus to monitoring.",
+            "Spearheaded multiple Proof of Concepts (PoCs) to ensure the best technologies and system design.",
+            "Presented findings and justifications for this choice to client representatives, effectively conveying the substantial improvements in code quality achievable through Sonarqube integration." 
         ]
     },
     {
-        company: "CI&T Software",
+        company: "True Work",
         role: "Software Engineer",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec",
+        startYear: 2021,
+        startMonth: "Apr",
+        endYear: 2021,
+        endMonth: "Nov",
         actions: [
-            "Created and maintained 10 web applications for numerous national and foreign clients.",
-            "Ensured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
-            "Created and analyzed 500 unit test cases.",
-            "Developed python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.",
-            "Established and lead a team of 10 people; covering every key role in the early stages."
+            "Led the Vue.js and Node.js front-end development for an IoT solution.",
+            "Managed the maintenance of backlog items, ensuring efficient task prioritization.",
+            "Oversaw gitflow processes, version control, and solution deployment across diverse environments.",
+            "Employed cutting-edge technologies such as Docker and Kubernetes to optimize system operations.",
+            "Provided guidance and mentorship to junior developers and interns, fostering their growth."
         ]
     },
     {
-        company: "CI&T Software",
+        company: "Inpelle Health Plan",
         role: "Software Engineer",
-        startYear: 2015,
-        startMonth: "Jan",
-        endYear: 2019,
-        endMonth: "Dec",
+        startYear: 2020,
+        startMonth: "Apr",
+        endYear: 2021,
+        endMonth: "Apr",
         actions: [
-            "Created and maintained 10 web applications for numerous national and foreign clients.",
-            "Ensured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
-            "Created and analyzed 500 unit test cases.",
-            "Developed python scripts to automate image's noise-reduction process which helped improve research analysis time by 40%.",
-            "Established and lead a team of 10 people; covering every key role in the early stages."
+            "Led the development of software for managing private clinic plans.",
+            "Played a key role in defining, developing, and implementing the Progressive Web App (PWA) solution.",
+            "Leveraged the JavaScript stack, utilizing React and Node.js, along with Postgres for robust backend functionality.",
+            "Established a custom infrastructure on Digital Ocean, employing Docker, Amazon SES, and SNS for efficient communication.",
+            "Designed and implemented a backup architecture employing RSync, Shellscripting, and Crontab.",
+            "Took charge of architecting and developing the front-end using Vue.js for a module dedicated to clinic and office management, in collaboration with a partner company."
+        ]
+    },
+    {
+        company: "KMM",
+        role: "Full Stack Developer",
+        startYear: 2019,
+        startMonth: "May",
+        endYear: 2020,
+        endMonth: "Apr",
+        actions: [
+            "Managed software maintenance across diverse technologies including Delphi, Java, and Oracle.",
+            "Act in the creation of innovative Java applications, expanding the functionality and capabilities of the system.",
+            "Leveraged the JavaScript stack, utilizing React and Node.js, along with Postgres for robust backend functionality.",
+            "Established a custom infrastructure on Digital Ocean, employing Docker, Amazon SES, and SNS for efficient communication.",
+            "Designed and implemented a backup architecture employing RSync, Shellscripting, and Crontab.",
+            "Took charge of architecting and developing the front-end using Vue.js for a module dedicated to clinic and office management, in collaboration with a partner company."
+        ]
+    },
+    {
+        company: "Ponta Grossa State University",
+        role: "Full Stack Developer",
+        startYear: 2016,
+        startMonth: "Oct",
+        endYear: 2018,
+        endMonth: "Oct",
+        actions: [
+            "Took charge of maintaining several institutional websites for UEPG using WordPress, ensuring their seamless functionality and up-to-date content.",
+            "Orchestrated the end-to-end development of an event registration platform for the Higher Education Teaching Program using Laravel, enhancing administrative efficiency.",
+            "Played an integral role in crafting a comprehensive event management system for the university, employing Laravel to streamline event-related processes."
         ]
     },
     ],
     technicalSkills: [
-        "JavaScript",
-        "HTML",
-        "CSS",
-        "React",
-        "Node.js",
-        "Express",
-        "paralelepipde"
+        "Node.js", "Java", "Javascript", "Vue.js", "Nuxt.js", "Cloud computing", "Docker", "Kubernetes", "Istio", "MongoDB", "Redis", "Scalability", "System Design", "Algorithms", "Distributed Systems"
     ],
     softSkills: [
-        "JavaScript",
-        "HTML",
-        "CSS",
-        "React",
-        "Node.js",
-        "Express",
-        "paralelepipde"
+        "Decision-making", "Ideas organizer", "Creative thinker", "self-motivated", "Proactive in taking ownership and responsability",
+        "Problem Solver"
     ],
     additionalSkills: [
-        "Public Speaking",
-        "Writting"
+        "Competitive programming"
     ],
     languages: [
         "English",
